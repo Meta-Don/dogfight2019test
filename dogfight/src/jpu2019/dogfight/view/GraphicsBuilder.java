cd6d99af3ce77f70d78495db1daaa24a3f7c265b
ap-am <ap-am@AMB-ROG>
2019-05-02 10:25:11 +0200

    soupe

--- dogfight/src/jpu2019/dogfight/view/GraphicsBuilder.java
+++ dogfight/src/jpu2019/dogfight/view/GraphicsBuilder.java
@@ -1,5 +1,8 @@
 package jpu2019.dogfight.view;
 
+import jpu2019.dogfight.model.IDogfightModel;
+import java.awt.image.BufferedImage;
+
 public class GraphicsBuilder {
 
 	public void GraphicsBuilder(IDogfightModel dogfightModel) {
