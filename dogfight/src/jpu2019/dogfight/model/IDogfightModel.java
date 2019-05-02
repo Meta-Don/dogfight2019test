41b825b9560244c72d886d30ae8b66e85b40be16
Meta-Don <Meta-Don@AMB.Poulet>
2019-05-02 11:02:20 +0200

    corrections en cours sur model

--- dogfight/src/jpu2019/dogfight/model/IDogfightModel.java
+++ dogfight/src/jpu2019/dogfight/model/IDogfightModel.java
@@ -1,5 +1,19 @@
 package jpu2019.dogfight.model;
 
 public interface IDogfightModel {
-
+	
+	public abstract IArea getArea();
+	
+	public abstract void buildArea(Dimension dimension);
+	
+	public abstract void addMobile(IMobile mobile);
+	
+	public abstract void removeMobile(IMobile mobile);
+	
+	public abstract ArrayList<IMobile> getMobile();
+	
+	public abstract IMobile getMobileByPlayer(int player);
+	
+	public abstract void setMobilesHaveMoved();
+	
 }
