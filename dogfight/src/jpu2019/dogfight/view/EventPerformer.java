67b11ce69a7386cd2ec39fcb6e1b4891123c0e53
ap-am <ap-am@AMB-ROG>
2019-05-02 10:28:20 +0200

    aaah

--- dogfight/src/jpu2019/dogfight/view/EventPerformer.java
+++ dogfight/src/jpu2019/dogfight/view/EventPerformer.java
@@ -2,19 +2,21 @@ package jpu2019.dogfight.view;
 
 import jpu2019.dogfight.controller.IOrderPerformer;
 import jpu2019.dogfight.controller.UserOrder;
+import jpu2019.gameframe.IEventPerformer;
 
-public class EventPerformer {
+public abstract class EventPerformer implements IEventPerformer {
 	
 	public EventPerformer(IOrderPerformer orderPerformer) {
 		
 	}
 	
+	@Override
 	public void eventPerform(KeyEvent keyCode) {
 		
 	}
 	
 	public UserOrder keyCodeToUserOrder(int keyCode) {
-		
+		return ;
 	}
 	
 }
